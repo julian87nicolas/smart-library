7bcd1f00d2a1a47a55acce926d7da2b892fb74c8
Julian Camargo <julicmrgo@gmail.com>
2025-05-08 12:47:56 -0300

    fix test

--- src/main/java/com/books/smartlibrary/SmartLibraryApplication.java
+++ src/main/java/com/books/smartlibrary/SmartLibraryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartLibraryApplication {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         SpringApplication.run(SmartLibraryApplication.class, args);
     }
 
