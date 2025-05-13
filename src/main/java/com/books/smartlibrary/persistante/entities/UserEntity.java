662689a629284f1b1d756f19fd97e74b561db907
Julian Camargo <julicmrgo@gmail.com>
2025-05-13 17:32:55 -0300

    feat: user entity

---
+++ src/main/java/com/books/smartlibrary/persistante/entities/UserEntity.java
@@ -0,0 +1,41 @@
+package com.books.smartlibrary.persistante.entities;
+
+import com.books.smartlibrary.domain.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="authors")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    Integer userId;
+    String id;
+    String name;
+    String lastName;
+    Gender gender;
+
+    @ManyToOne
+    RoleEntity role;
+
+    @OneToMany
+    List<OrderEntity> orders;
+
+
+
+    public UserEntity(String id, String name, String lastName, RoleEntity role, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.role = role;
+        this.gender = gender;
+    }
+}
